26ad9fce9f993449cc01da0099c33cbba547cf41
harshini787 <harshinimohanakannan@gmail.com>
2025-08-22 16:13:31 +0530

    the error rectified

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from blogapp import views  # Import your app's views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Root URL for the slider
+    path('register/', views.register, name='register'),  # Register page
+    path('collections/', views.collections, name='collections'),  # Collections page
+]
 
 from django.conf import settings
 from django.conf.urls.static import static 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('blogapp.urls')),  
-]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-   
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,31 +1,12 @@
 from django.shortcuts import render
-from . models import *
+from .models import Blog
 
 def home(request):
-   
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html')  # Root page with slider content
 
 def register(request):
-    
-    return render(request, 'blog/register.html')
+    return render(request, 'blog/register.html')  # Register page
 
 def collections(request):
-    categories = Blog.CATEGORY_CHOICES   # Get all categories
-    return render(request, 'blog/collections.html', {'categories': categories})
-
-
-def collection(request):
-    blogs = Blog.objects.all()
-
-    category_defaults = {
-        "food": "images/defaults/food.webp",
-        "travel": "images/defaults/travel.jpg",
-        "tech": "images/defaults/TECHNOLOGY.jpg",
-        "lifestyle": "images/defaults/lifestyle.webp",
-        "education": "images/defaults/edu.jpg",
-    }
-
-    return render(request, "collection.html", {
-        "blogs": blogs,
-        "category_defaults": category_defaults
-    })
+    blogs = Blog.objects.all()  # Fetch all blogs
+    return render(request, 'blog/collections.html', {'blogs': blogs})  # Collections page
